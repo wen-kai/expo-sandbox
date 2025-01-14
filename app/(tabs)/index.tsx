328baa174830f31281b5ef2af767129bf79b70c1
Marc Bourguignon <wen-kai@hotmail.com>
2025-01-14 10:37:10 -0700

    scale bug for ios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,61 +10,39 @@ import {useState} from "react";
 const { width, height } = Dimensions.get('window');
 
 export default function HomeScreen() {
-  const [fullscreenIndex, setFullScreenIndex] = useState(null);
-  const [viewableIndex, setViewableIndex] = useState(0);
-  const [viewableIndexNested, setViewableIndexNested] = useState(0);
-  const [shouldPlay, setShouldPlay] = useState(true);
 
   return (
     <View
       style={{ flex: 1, backgroundColor: 'black' }}
     >
-    <View style={{ width, height }}>
-        <FlatList
-            data={[
-                {
-                    video1: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4',
-                    video2: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-                },
-                {
-                    video1: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-                    video2: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-                },
-                {
-                    video1: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-                    video2: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-                }
-            ]}
-            snapToOffsets={[0, height, height * 2]}
-            snapToAlignment={'start'}
-            onViewableItemsChanged={({ viewableItems }) => {
-                const viewableItem = viewableItems.find(i => i.isViewable);
-                setViewableIndex(viewableItem?.index);
+        <View
+            style={{
+                position: 'absolute',
+                top: 100,
+                left: 100,
+                transform: [
+                    { scale: 100 },
+                ],
             }}
-            renderItem={({ item, index }) => (
-                <View style={{ width, height }}>
-                    <VideoPlayer
-                        style={{ width, height }}
-                        shouldPlay={shouldPlay && index === viewableIndex}
-                        isFullscreen={fullscreenIndex === index}
-                        onFullscreenExit={() => setFullScreenIndex(null)}
-                        source={item.video1}
-                    />
-
-                    {/* FULLSCREEN & PLAY BUTTONS */}
-                    <View style={{ position: 'absolute', bottom: 50, right: 50 }}>
-                        <TouchableOpacity onPress={() => setFullScreenIndex(index)}>
-                            <ThemedText type="title">Fullscreen</ThemedText>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => setShouldPlay(!shouldPlay)}>
-                            <ThemedText type="title">{shouldPlay ? 'Pause' : 'Play'}</ThemedText>
-                        </TouchableOpacity>
-                    </View>
+        >
+            <View
+                style={{
+                    transform: [
+                        { scale: 0.01 },
+                    ],
+                }}
+            >
+                <View
+                    style={{
+                        transform: [
+                            { scale: 1 },
+                        ],
+                    }}
+                >
+                    <View style={{ width: 100, height: 100, borderWidth: 3, borderColor: 'blue', backgroundColor: 'red' }} />
                 </View>
-            )}
-        />
-    </View>
+            </View>
+        </View>
     </View>
   );
 }
